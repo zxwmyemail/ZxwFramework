c490ae806f6288f844e7b117a0db7a89aad1e3f1
zxwmyemail <1440552721@qq.com>
2015-03-22 11:01:17 +0800

    Create MyTest.php

---
+++ public/MyTest.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('BASE_PATH'))
+    exit('<H2 style="margin-top:200px;text-align:center;">Your request was forbidden!</H2>');
+
+class MyTest {
+
+    public function __construct() {
+        echo "my lib test";
+    }
+    
+}
