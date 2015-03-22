8327986949f8277f653e214e28801c6130dac640
iProg <1440552721@qq.com>
2015-03-22 20:23:27 +0800

    Update HttpRequest.php

--- system/library/HttpRequest.php
+++ system/library/HttpRequest.php
@@ -20,7 +20,7 @@ class httpRequest {
 
 	}
 
-   /*------------------------------------------------------------------------------
+   	/*------------------------------------------------------------------------------
 	| 发起请求
 	-----------------------------------------------------------------------------*/
 	public function doHttpRequest($url, $param=array()){
@@ -40,63 +40,63 @@ class httpRequest {
 
 	}
 
-   /*------------------------------------------------------------------------------
+   	/*------------------------------------------------------------------------------
 	| 发起get请求
 	-----------------------------------------------------------------------------*/
 	private function get($url, $param=array())
 	{
-     	if(!is_array($param)){
-         	throw new Exception("参数必须为array");
-     	}
+     		if(!is_array($param)){
+         		throw new Exception("参数必须为array");
+     		}
 
-     	$p='';
-     	foreach($param as $key => $value){
-         	$p=$p.$key.'='.$value.'&';
-     	}
+	     	$p='';
+	     	foreach($param as $key => $value){
+	         	$p=$p.$key.'='.$value.'&';
+	     	}
 
-     	if(preg_match('/\?[\d\D]+/',$url)){
-         	$p='&'.$p;
-     	}else if(preg_match('/\?$/',$url)){
-         	$p=$p;
-     	}else{
-         	$p='?'.$p;
-     	}
-     	$p=preg_replace('/&$/','',$p);
-     	$url=$url.$p;
-     	//echo $url;
-     	$httph =curl_init($url);
-     	curl_setopt($httph, CURLOPT_SSL_VERIFYPEER, 0);
-     	curl_setopt($httph, CURLOPT_SSL_VERIFYHOST, 1);
-     	curl_setopt($httph,CURLOPT_RETURNTRANSFER,1);
-     	curl_setopt($httph, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-     
-     	curl_setopt($httph, CURLOPT_RETURNTRANSFER,1);
-     	curl_setopt($httph, CURLOPT_HEADER,1);
-     	$rst=curl_exec($httph);
-     	curl_close($httph);
-     	return $rst;
+	     	if(preg_match('/\?[\d\D]+/',$url)){
+	         	$p='&'.$p;
+	     	}else if(preg_match('/\?$/',$url)){
+	         	$p=$p;
+	     	}else{
+	         	$p='?'.$p;
+	     	}
+	     	$p=preg_replace('/&$/','',$p);
+	     	$url=$url.$p;
+	     	//echo $url;
+	     	$httph =curl_init($url);
+	     	curl_setopt($httph, CURLOPT_SSL_VERIFYPEER, 0);
+	     	curl_setopt($httph, CURLOPT_SSL_VERIFYHOST, 1);
+	     	curl_setopt($httph,CURLOPT_RETURNTRANSFER,1);
+	     	curl_setopt($httph, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+	     
+	     	curl_setopt($httph, CURLOPT_RETURNTRANSFER,1);
+	     	curl_setopt($httph, CURLOPT_HEADER,1);
+	     	$rst=curl_exec($httph);
+	     	curl_close($httph);
+	     	return $rst;
  	}
 
-   /*------------------------------------------------------------------------------
+   	/*------------------------------------------------------------------------------
 	| 发起post请求
 	-----------------------------------------------------------------------------*/
  	private function post($url, $param=array())
  	{
-     	if(!is_array($param)){
-         	throw new Exception("参数必须为array");
-     	}
-     	$httph =curl_init($url);
-     	curl_setopt($httph, CURLOPT_SSL_VERIFYPEER, 0);
-     	curl_setopt($httph, CURLOPT_SSL_VERIFYHOST, 1);
-     	curl_setopt($httph,CURLOPT_RETURNTRANSFER,1);
-     	curl_setopt($httph, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-     	curl_setopt($httph, CURLOPT_POST, 1);
-     	curl_setopt($httph, CURLOPT_POSTFIELDS, $param);
-     	curl_setopt($httph, CURLOPT_RETURNTRANSFER,1);
-     	curl_setopt($httph, CURLOPT_HEADER,1);
-     	$rst=curl_exec($httph);
-     	curl_close($httph);
-     	return $rst;
+	     	if(!is_array($param)){
+	         	throw new Exception("参数必须为array");
+	     	}
+	     	$httph =curl_init($url);
+	     	curl_setopt($httph, CURLOPT_SSL_VERIFYPEER, 0);
+	     	curl_setopt($httph, CURLOPT_SSL_VERIFYHOST, 1);
+	     	curl_setopt($httph,CURLOPT_RETURNTRANSFER,1);
+	     	curl_setopt($httph, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+	     	curl_setopt($httph, CURLOPT_POST, 1);
+	     	curl_setopt($httph, CURLOPT_POSTFIELDS, $param);
+	     	curl_setopt($httph, CURLOPT_RETURNTRANSFER,1);
+	     	curl_setopt($httph, CURLOPT_HEADER,1);
+	     	$rst=curl_exec($httph);
+	     	curl_close($httph);
+	     	return $rst;
  	}
 }
 
