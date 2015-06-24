018ef696f44f92643d710b98f533e302d275646e
iProg <1440552721@qq.com>
2015-06-24 11:19:38 +0800

    Update jsonRpcClientController.php

--- mvc/controller/homeModule/jsonRpcClientController.php
+++ mvc/controller/homeModule/jsonRpcClientController.php
@@ -7,13 +7,13 @@
 class jsonRpcClientController extends Controller {
     
     public function index(){
-        // 导入客户端类库
-        $client = Application::newObject('jsonRPCClient', 'jsonRPC');
-        $client->setJsonRPCServerUrl('http://localhost/index.php?c=jsonRpcServer&a=index');
+        
+        $this->jsonRPCClient->setJsonRPCServerUrl('http://localhost/index.php?c=jsonRpcServer&a=index');
         $result = $client->index();
         var_dump($result); // 结果：Hello, JsonRPC!
-        $result = $client->test('ThinkPHP');
-        var_dump($result); // 结果：Hello, ThinkPHP!
+        $result = $client->test('ZxwFramework');
+        var_dump($result); // 结果：Hello, ZxwFramework!
+        
     }
     
 }
