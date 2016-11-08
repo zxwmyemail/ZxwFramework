50111a3f9817293e83c3af5e033894d8e2e9706a
iProg <1440552721@qq.com>
2016-11-08 18:58:26 +0800

    Update Verify.php

--- system/library/Verify.php
+++ system/library/Verify.php
@@ -1,8 +1,6 @@
-<?php
-	
+<?php	
 if (!defined('BASE_PATH'))
 	exit('<H2 style="margin-top:200px;text-align:center;">Your request was forbidden!</H2>');
-
 /***************************************************************************************************
  图片验证码类 Verify
 
